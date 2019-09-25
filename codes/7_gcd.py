4bc86462fa6d0d3541f30d48b09d5639d5405573
Vikas Patel <vikaspatelp83@gmail.com>
2019-09-25 22:00:42 +0530

    updated gcd code

--- codes/7_gcd.py
+++ codes/7_gcd.py
@@ -1,13 +1,14 @@
 def gcd(a,b):
+    # choose the least number
+    if a > b:
+        least = b
+    else:
+        least = a
+    for i in range(1, least + 1):
+        if ((a % i == 0) and (b % i == 0)):
+            gcd = i #or hcf
+
+    return gcd
 
 
-    # if a<b :
-    #     a,b=b,a
-    #     while b!=0 :
-    #         r = b
-    #         b = a%r
-    #
-    #         return a
-
-
-print(gcd(2,22))
+print(gcd(11,22))
