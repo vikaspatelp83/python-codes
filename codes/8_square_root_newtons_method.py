8c932b13895f1d8825065897d0f29a1cdbe845bf
Vikas Patel <vikaspatelp83@gmail.com>
2019-09-25 22:30:41 +0530

    newtons method of square root

---
+++ codes/8_square_root_newtons_method.py
@@ -0,0 +1,17 @@
+
+def average(a, b):
+    return (a + b) / 2.0
+def improve(guess, x):
+    return average(guess, x/guess)
+def good_enough(guess, x):
+    d = abs(guess*guess - x)
+    return (d < 0.001)
+def square_root(guess, x):
+    while(not good_enough(guess, x)):
+        guess = improve(guess, x)
+    return guess
+def my_sqrt(x):
+    r = square_root(1, x)
+    return r
+
+print(my_sqrt(4))
